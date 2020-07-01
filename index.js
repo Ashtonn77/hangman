38e5409a6cf421d5bd86c6e46d03881b5765340f
Ashtonn77 <ashtonrbay@gmail.com>
2020-07-01 21:29:05 +0200

    updated pop notifications functions

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const words = ["professional", "engineer", "wizard", "serendipity"];
 
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 
-let correctLetters = ["w", "i", "z", "a", "r", "d"];
+let correctLetters = [];
 let wrongLetters = [];
 
 function displayWord() {
@@ -36,5 +36,43 @@ function displayWord() {
     popUp.style.display = "flex";
   }
 }
+
+//update wrong leters
+function updateWrongLettersEl() {
+  console.log("Update wrong");
+}
+
+//show pop up notification
+function showNotification() {
+  notification.classList.add("show");
+
+  setTimeout(() => {
+    notification.classList.remove("show");
+  }, 2000);
+}
+
+//keydown letter press
+window.addEventListener("keydown", (e) => {
+  if (e.keyCode >= 65 && e.keyCode <= 90) {
+    const letter = e.key;
+
+    if (selectedWord.includes(letter)) {
+      if (!correctLetters.includes(letter)) {
+        correctLetters.push(letter);
+        displayWord();
+      } else {
+        showNotification();
+      }
+    } else {
+      if (!wrongLetters.includes(letter)) {
+        wrongLetters.push(letter);
+
+        updateWrongLettersEl();
+      } else {
+        showNotification();
+      }
+    }
+  }
+});
 
 displayWord();
